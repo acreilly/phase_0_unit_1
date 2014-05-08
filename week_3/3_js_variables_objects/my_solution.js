2fb5bed76396445ccfc025bd39e475a16857ab67
Allie Reilly <acreilly3@aol.com>
2014-05-08 16:17:22 -0400

    Update my_solution.js

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,19 +1,29 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself] on this challenge.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+//  test 1 - the secret number has to be a number so I will define the variable as a number, I will choose 7 to make test 2 work
+// test 2 -  secret number variable has been defined 7
+// test 3 - the password should be a string so i will define the variable as a string
+// test 4 - define the variable
+// test 5 -I will define allowedIn variable as a boolean
+// test 6 - define allowedIn
+//test 7 -I will define members as an array
+//test 8 -assign first element in array as john
+// test 9 - assign fourth element in array as mary
 
 
 // __________________________________________
-// Write your code below.
+// // Write your code below.
 
-
+// var secretNumber = 7;
+// var password = "just open the door";
+// var allowedIn = false;
+// var members = [];
+// members[0] = "John";
+// members[3] = "Mary";
 
 
 
@@ -22,7 +32,10 @@
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", , ,"Mary"];
 
 
 
